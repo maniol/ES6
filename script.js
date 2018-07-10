609239f2c69d850fd27e81795ccdc2a53aa73476
magda <magdalena.aniol@gmail.com>
2018-07-10 12:19:05 +0200

    add class Stopwatch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Stopwatch extends React.Component {
+Stopwatch = class Stopwatch extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { running: false };
@@ -20,12 +20,7 @@ class Stopwatch extends React.Component {
 	format = (times) => {
 		return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
 	}
-	start = () => {
-		if (this.state != running) {
-			this.setState({running: true});
-			this.watch = setInterval(() => this.step(), 10);
-		}
-	}
+
 	step = () => {
 		if (this.state != running) return;
 		this.calculate();
@@ -42,27 +37,12 @@ class Stopwatch extends React.Component {
 				this.times.seconds = 0;
 		}
 	}
-	stop = () => {
-		this.setState({running = false});
-		clearInterval(this.watch);
-	}
-	resetTimes = () => {
-		this.stop()
-		this.recordTime();
-		this.printRecordList();
-		this.reset();
-		this.print();
-	}
 	render () {
 		return(
 			<div className={"stopwatch"}>this.format(this.time)</div>
 			)
 	}
 }
-
-const stopwatch = React.createElement(Stopwatch);
-ReactDOM.render(stopwatch, document.getElementById('stopwatch'));
-
 function pad0(value) {
 	let result = value.toString();
 	if (result.length < 2) {
@@ -70,3 +50,7 @@ function pad0(value) {
 	}
 	return result;
 }
+
+const display = document.getElementById('stopwatch');
+const stopwatch = React.createElement(Stopwatch(display));
+ReactDOM.render(stopwatch, document.getElementById('stopwatch'));
