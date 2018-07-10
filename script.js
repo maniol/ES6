07b2752b5c583e6791269aa1d23b4da0f3dd1577
magda <magdalena.aniol@gmail.com>
2018-07-10 11:43:06 +0200

    refactor stopwatch class

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-class Stopwatch {
-	constructor(display) {
-		this.running = false;
-		this.display = display;
-		this.recordList = [];
+class Stopwatch extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { running: false };
 		this.reset();
-		this.print(this.times);
 	}
-	reset() {
+	static defaultProps = {
+		display: display
+	}
+	static propTypes = {
+		display: React.PropTypes.object.isRequired
+	}
+	reset = () => {
 		this.times = {
 			minutes: 0,
 			seconds: 0,
 			miliseconds:0
 		};
 	}
-	print() {
-		this.display.innerText = this.format(this.times);
-	}
-	format(times) {
+	format = (times) => {
 		return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
 	}
-	start() {
-		if (!this.running) {
-			this.running =true;
+	start = () => {
+		if (this.state != running) {
+			this.setState({running: true});
 			this.watch = setInterval(() => this.step(), 10);
 		}
 	}
-	step() {
-		if (!this.running) return;
+	step = () => {
+		if (this.state != running) return;
 		this.calculate();
 		this.print();
 	}
-	calculate() {
+	calculate = () => {
 		this.times.miliseconds += 1;
 		if (this.times.miliseconds >= 100) {
 				this.times.seconds += 1;
@@ -41,46 +42,26 @@ class Stopwatch {
 				this.times.seconds = 0;
 		}
 	}
-	stop() {
-		this.running = false;
+	stop = () => {
+		this.setState({running = false});
 		clearInterval(this.watch);
 	}
-	resetTimes() {
+	resetTimes = () => {
 		this.stop()
 		this.recordTime();
 		this.printRecordList();
 		this.reset();
 		this.print();
 	}
-	recordTime() {
-		this.recordList.push(this.times);
-	}
-	resetRecordList() {
-		this.recordList = [];
-		this.printRecordList();
-	}
-	printRecordList() {
-		let results = document.getElementById('results');
-		let resultsListHTML = '';
-		for(let i = 0; i < this.recordList.length; i++) {
-			let j = i + 1; 
-			resultsListHTML += `<li>${j}. ${this.format(this.recordList[i])}</li>`
-		}
-		results.innerHTML = resultsListHTML;
+	render () {
+		return(
+			<div className={"stopwatch"}>this.format(this.time)</div>
+			)
 	}
 }
 
-const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
-
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start());
-let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop());
-let resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => stopwatch.resetTimes());
-let resetRecordListButton = document.getElementById('reset-record-list');
-resetRecordListButton.addEventListener('click', () => stopwatch.resetRecordList());
+const stopwatch = React.createElement(Stopwatch);
+ReactDOM.render(stopwatch, document.getElementById('stopwatch'));
 
 function pad0(value) {
 	let result = value.toString();
