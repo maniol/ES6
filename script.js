3bb856cb2dec59f59bf62e5caf03b6b504c6b0ce
magda <magdalena.aniol@gmail.com>
2018-07-11 13:10:34 +0200

    fix render location add styles

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
-
 class App extends React.Component {
 	constructor() {
 		super();
@@ -49,7 +44,7 @@ class UsersList extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<div className="record-list">
 				{this.users}
 			</div>
 		);
@@ -59,10 +54,15 @@ class UsersList extends React.Component {
 class User extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className="listing">
 				<img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
 				<a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
 			</div>
 		);
 	}
 }
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
